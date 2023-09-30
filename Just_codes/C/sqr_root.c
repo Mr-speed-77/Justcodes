29d51c5f530b70a116b90e0f00177078d432eafb
Sathish_77 <132646947+Mr-speed-77@users.noreply.github.com>
2023-09-30 11:25:35 +0530

    Create sqr_root.c

---
+++ Just_codes/C/sqr_root.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h> // using sqrt() function
+
+
+int main(){
+    
+    float num;
+    printf("Enter any number: ");
+    scanf("%f",&num);
+    
+    // Find square root 
+    printf("Square root = %.1f",sqrt(num));
+}
+
+
+
