b193c2bc3a82367646b7e2626cc431fe4aeb1905
Sathish_77 <132646947+Mr-speed-77@users.noreply.github.com>
2023-09-30 09:07:38 +0530

    rectangle.c

---
+++ Just_codes/C/rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+  // Declare a varialble
+    float length,width,rectangle;
+    printf("Enter a length: ");
+    scanf("%f",&length);
+    printf("Enter a width: ");
+    scanf("%f",&width);
+
+    // Using the formula for Rectangle
+    rectangle =2*(length + width);
+    printf("Perimeter of rectangle = %f units",rectangle);
+    
+    
+}
