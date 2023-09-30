4354eb59a0b137ab0e66891f90676cf45718c32c
Sathish_77 <132646947+Mr-speed-77@users.noreply.github.com>
2023-09-30 11:09:40 +0530

    Update area_of_triangle.c

--- Just_codes/C/area_of_triangle.c
+++ Just_codes/C/area_of_triangle.c
@@ -3,16 +3,16 @@
 
 int main(){
     
-    double height,base,area;
+    float height,base,area;
     printf("Enter a height: ");
-    scanf("%lf",&height);
+    scanf("%f",&height);
     printf("Enter a base: ");
-    scanf("%lf",&base);
+    scanf("%f",&base);
     
     // Area of triangel 
     
     area = (height * base)/2;
-    printf("Area of the triangle: %.2lf sq. units\n",area);
+    printf("Area of the triangle: %.2f sq. units\n",area);
     
     
 }
